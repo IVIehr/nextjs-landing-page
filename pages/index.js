cba90ce54b7b1abc87c865aad91a943b537b5bf5
mehrnoosh <mehr910093@gmail.com>
2022-08-24 15:42:22 +0430

    Add 'features' component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Layout from '../components/layout'
 import Hero from '../components/Hero'
+import Features from './features'
 
 export default function Home() {
   return (
     <div>
       <Layout>
         <Hero/>
+        <Features/>
       </Layout>
     </div>
   )
