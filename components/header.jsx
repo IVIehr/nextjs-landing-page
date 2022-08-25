3f37b23a3be93381de791af1b1ba8a38eb8e516a
mehrnoosh <mehr910093@gmail.com>
2022-08-25 12:26:49 +0430

    Use 'react-scroll' to anchor to a page section without changing url

--- components/header.jsx
+++ components/header.jsx
@@ -3,13 +3,14 @@
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { Fragment } from "react";
-import Link from 'next/link';
+import LinkNext from 'next/link';
+import { Link } from 'react-scroll';
 
 
 const Header = ({featuresId}) => {
   const navigation = [
+      { name: "Features", href: featuresId },
       { name: "Product", href: "/products" },
-      { name: "Features", href: `#${featuresId}` },
       { name: "Marketplace", href: "/marketplace" },
       { name: "Company", href: "/company" }
   ];
@@ -35,12 +36,12 @@ const Header = ({featuresId}) => {
               >
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
-                    <Link href="/">
+                    <LinkNext href="/">
                       <img
                         className="h-8 w-auto sm:h-10 cursor-pointer"
                         src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                       />
-                    </Link>
+                    </LinkNext>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-background-main rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Open main menu</span>
@@ -51,17 +52,15 @@ const Header = ({featuresId}) => {
                 </div>
                 <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                   {navigation.map((item) => (
-                    <Link href={item.href} key={item.name}>
-                      <a className="font-medium text-gray-500 hover:text-gray-900">
+                    <Link to={item.href} key={item.name} className="font-medium text-gray-500 hover:text-gray-900">
                         {item.name}
-                      </a>
                     </Link>
                   ))}
-                  <Link href="/login">
+                  <LinkNext href="/login">
                     <a className="font-medium text-gray-500 hover:text-gray-900">
                       Login
                     </a>
-                  </Link>
+                  </LinkNext>
                 </div>
               </nav>
             </div>
@@ -97,18 +96,16 @@ const Header = ({featuresId}) => {
                   </div>
                   <div className="px-2 pt-2 pb-3 space-y-1">
                     {navigation.map((item) => (
-                      <Link key={item.name} href={item.href}>
-                        <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
+                      <Link key={item.name} to={item.href} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
                           {item.name}
-                        </a>
                       </Link>
                     ))}
                   </div>
-                  <Link href="/login">
+                  <LinkNext href="/login">
                     <a className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
                       Log in
                     </a>
-                  </Link>
+                  </LinkNext>
                 </div>
               </Popover.Panel>
             </Transition>
