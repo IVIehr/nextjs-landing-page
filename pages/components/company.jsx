645871b6277d713d427ff73239eabf077ab7cbe9
mehrnoush.navidi <mehr910093@gmail.com>
2023-04-15 11:38:51 +0430

    Add company component

--- pages/components/company.jsx
+++ pages/components/company.jsx
@@ -1,17 +1,45 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Image from 'next/image'
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 export default function Company({id}) {
+  // const userState = useSelector((userState) => userState.handleUser);
+  // const navigate = useNavigate();
+
+  // const proceedTeaching = () => {
+  //   if (userState !== null) {
+  //     navigate("/cooperation");
+  //   } else {
+  //     toast("لطفا ابتدا وارد شوید");
+  //   }
+  // };
   return (
-    <div className="mb-12 py-12 bg-white" id={id}>
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center py-10">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Company
-          </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            these are our Company
-          </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos est totam tempora maxime odio, corporis expedita temporibus sint voluptate consequuntur quos dolores nostrum repellat accusantium laudantium explicabo doloremque culpa. Iusto?
-          </p>
+    <div id={id}>
+      <div className="container py-5 my-5">
+        <div className="grid grid-cols-2 gap-4">
+        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+          <img
+          src="teach.png"
+          alt="company"
+          width={500}
+          height={500}
+        />
+        </div>
+          <div className="lg:text-center py-10">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              Company
+            </h2>
+            <p className="leading-7 my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo alias porro accusantium doloribus soluta vitae, tempora quis velit tempore iure possimus dignissimos, saepe a deleniti ipsam nihil voluptatibus sunt, omnis fugit incidunt. Inventore molestias modi, voluptate quaerat ipsa, praesentium mollitia velit sed hic, tempore quibusdam.</p>
+            <button
+            // onClick={proceedTeaching}
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-2 w-50"
+          >
+            join us
+          </button>
+          <ToastContainer />
+          </div>
         </div>
       </div>
     </div>
